e75f7d6c1194a8ffec052d0b803a1194c44f987a
zhuqiuyang <zqyhit@qq.com>
2020-07-19 15:27:30 +0800

    add debian init & ssh config

---
+++ deb.sh
@@ -0,0 +1,36 @@
+### 1. root
+
+apt-get update
+
+apt-get upgrade
+
+apt-get install sudo vim
+
+
+##2.  add user
+
+useradd ace
+
+mkdir /home/ace
+
+chown ace:ace /home/ace -R
+
+usermod -d /home/ace ace
+
+passwd ace
+
+vim /etc/sudoers
+
+### 3. switch user
+su ace
+
+
+### 4. zsh
+
+sudo apt-get intall git
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+###
+
+service ssh restart
