876105ff6de7ceb9e1fd935f46390e6ebdbefe87
zhuqiuyang <zqyhit@qq.com>
2020-10-21 21:42:12 +0800

    add docker-compose

--- install/docker.sh
+++ install/docker.sh
@@ -3,3 +3,6 @@ curl -sSL https://get.daocloud.io/docker | sh
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
 usermod -aG docker ace
+
+# install docker-compose
+wget https://github.com/docker/compose/releases/download/1.27.4/docker-compose-Linux-x86_64
